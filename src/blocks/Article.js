3a130f06c59dda753770744f0e7acae27c1f555a
Holger H <holgheiss@gmail.com>
2020-04-06 18:10:44 +0200

    Article <mark>

--- src/blocks/Article.js
+++ src/blocks/Article.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {Fragment} from "react"
 import styled from 'styled-components'
 
 import {p as P} from '../components/HtmlElements'
@@ -9,15 +9,34 @@ export function Article({ data }) {
   return (
     <>
       <LargeText>
-        {text}
+        {parseText(text)}
       </LargeText>
       <SmallText>
         {text2}
       </SmallText>      
-      filename: {file}
-      <DownloadLink text=".PDF" href={file} />
+      <DownloadLink title={file} text=".PDF" href={file} />
     </>
   )
+}
+
+const parseText = function(text) {
+  let out =[]
+  let groups = text
+    .split("{")
+    .map( g => g.split("}") )
+  if (groups.length > 0) {
+    for (let gg of groups) {
+      if (gg.length > 1) {
+        out.push(<mark>{gg[0]}</mark>)
+        out.push(<span>{gg[1]}</span>)
+      } else {
+        out.push(<span>{gg[0]}</span>)
+      }
+    }
+  }
+  console.log(out);
+  return out.map(c => <Fragment key="x">{c}</Fragment>)
+  //return text.replace(/\{/, '<mark>').replace(/\}/, '</mark>')  
 }
 
 const LargeText = styled.p`
@@ -33,9 +52,9 @@ export const ArticleBlock = {
   defaultItem: {
     text: `„It was due to the new system, that
         humanity redifined its relation to
-        nature. Today, <mark>nature is an active</mark>
+        nature. Today, {nature is an active
         political agent: land, water, air and
-        light cannot be owned anymore.“`,
+        light cannot be owned anymore.}“`,
     text2: 'Conversation with Eyal Weizman',
     file: ''
   },
